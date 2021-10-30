098161123b0c31231aaa446eb0de5bb8f8ab0b12
Crawford Vining <crawpdx@gmail.com>
2021-10-30 14:38:04 -0700

    updated board with xs and 0s. updated css. attepmted to addclasses to show hide player moves

--- src/Board.js
+++ src/Board.js
@@ -1,18 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Board(){
+    const [user, setUser] = useState(false)
+
+    function handleMove(){
+        if(user){
+            document.getElementById('move').classList.add('show')
+        } else {
+            document.getElementById('move').classList.add('show')
+        }
+    }
 
     return(
         <div className="board-flex">
-            <div className="square"><span className="play-piece eks">X</span></div>
-            <div className="square"></div>
-            <div className="square"></div>
-            <div className="square"></div>
-            <div className="square"></div>
-            <div className="square"></div>
-            <div className="square"></div>
-            <div className="square"></div>
-            <div className="square"><span className="play-piece zero">0</span></div>
+            <div id="square" onClick={handleMove}>
+                <span id="move" className="user-move">{user ? 'X' : '0'}</span>
+            </div>
+            <div id="square" onClick={handleMove}>
+                <span id="move" className="user-move">{user ? 'X' : '0'}</span>
+            </div>
+            <div id="square" onClick={handleMove}>
+                <span id="move" className="user-move">{user ? 'X' : '0'}</span>
+            </div>
+            <div id="square" onClick={handleMove}>
+                <span id="move" className="user-move">{user ? 'X' : '0'}</span>
+            </div>
+            <div id="square" onClick={handleMove}>
+                <span id="move" className="user-move">{user ? 'X' : '0'}</span>
+            </div>
+            <div id="square" onClick={handleMove}>
+                <span id="move" className="user-move">{user ? 'X' : '0'}</span>
+            </div>
+            <div id="square" onClick={handleMove}>
+                <span id="move" className="user-move">{user ? 'X' : '0'}</span>
+            </div>
+            <div id="square" onClick={handleMove}>
+                <span id="move" className="user-move">{user ? 'X' : '0'}</span>
+            </div>
+            <div id="square" onClick={handleMove}>
+                <span id="move" className="user-move">{user ? 'X' : '0'}</span>
+            </div>
         </div>
     )
 }
