04e39c5d9ff764e47400df8539a35440f41c2974
Crawford Vining <crawpdx@gmail.com>
2021-10-31 10:53:01 -0700

    brought array into component and I was able to display array

--- src/Board.js
+++ src/Board.js
@@ -1,13 +1,23 @@
 import React, { useState } from 'react'
-import Moves from './Moves'
+// import Moves from './Moves'
 
 export default function Board(){
     const [user, setUser] = useState(false)
-    const [playerMoves, setPlayerMoves] = useState([Moves])
+    // const [playerMoves, setPlayerMoves] = useState([Moves])
 
     // console.log(playerMoves)
 
-    
+    const Moves = [
+        { square: 1, player: 'X', computer: 'O', empty: null },
+        { square: 2, player: 'X', computer: 'O', empty: null },
+        { square: 3, player: 'X', computer: 'O', empty: null },
+        { square: 4, player: 'X', computer: 'O', empty: null },
+        { square: 5, player: 'X', computer: 'O', empty: null },
+        { square: 6, player: 'X', computer: 'O', empty: null }, 
+        { square: 7, player: 'X', computer: 'O', empty: null },
+        { square: 8, player: 'X', computer: 'O', empty: null },
+        { square: 9, player: 'X', computer: 'O', empty: null }
+    ]
 
     function handleMove(){
         if(user){
@@ -19,41 +29,42 @@ export default function Board(){
 
     return(
         <div className="board-flex">
+           <div id="square" onClick={handleMove}>
+                <span id="move" className="user-move">{user ? 'X' : '0'}</span>
+            </div>
+            <div id="square" onClick={handleMove}>
+                <span id="move" className="user-move">{user ? 'X' : '0'}</span>
+            </div>
+            <div id="square" onClick={handleMove}>
+                <span id="move" className="user-move">{user ? 'X' : '0'}</span>
+            </div>
+            <div id="square" onClick={handleMove}>
+                <span id="move" className="user-move">{user ? 'X' : '0'}</span>
+            </div>
+            <div id="square" onClick={handleMove}>
+                <span id="move" className="user-move">{user ? 'X' : '0'}</span>
+            </div>
+            <div id="square" onClick={handleMove}>
+                <span id="move" className="user-move">{user ? 'X' : '0'}</span>
+            </div>
+            <div id="square" onClick={handleMove}>
+                <span id="move" className="user-move">{user ? 'X' : '0'}</span>
+            </div>
+            <div id="square" onClick={handleMove}>
+                <span id="move" className="user-move">{user ? 'X' : '0'}</span>
+            </div>
+            <div id="square" onClick={handleMove}>
+                <span id="move" className="user-move">{user ? 'X' : '0'}</span>
+            </div>
             <div>
                 <ul>
-                 {playerMoves.map(move => {
-                     return(
-                        <li key={move.square}>{move.player}</li>
+                 {Moves.map(move => (
+                        <li style={{color:'#fff' }} key={move.square}>
+                            {move.player}, {move.computer}, {move.square}
+                        </li>
                      )
-                 })}
+                 )}
                 </ul>
-            </div>
-            <div id="square" onClick={handleMove}>
-                <span id="move" className="user-move">{user ? 'X' : '0'}</span>
-            </div>
-            <div id="square" onClick={handleMove}>
-                <span id="move" className="user-move">{user ? 'X' : '0'}</span>
-            </div>
-            <div id="square" onClick={handleMove}>
-                <span id="move" className="user-move">{user ? 'X' : '0'}</span>
-            </div>
-            <div id="square" onClick={handleMove}>
-                <span id="move" className="user-move">{user ? 'X' : '0'}</span>
-            </div>
-            <div id="square" onClick={handleMove}>
-                <span id="move" className="user-move">{user ? 'X' : '0'}</span>
-            </div>
-            <div id="square" onClick={handleMove}>
-                <span id="move" className="user-move">{user ? 'X' : '0'}</span>
-            </div>
-            <div id="square" onClick={handleMove}>
-                <span id="move" className="user-move">{user ? 'X' : '0'}</span>
-            </div>
-            <div id="square" onClick={handleMove}>
-                <span id="move" className="user-move">{user ? 'X' : '0'}</span>
-            </div>
-            <div id="square" onClick={handleMove}>
-                <span id="move" className="user-move">{user ? 'X' : '0'}</span>
             </div>
         </div>
     )
