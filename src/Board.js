203bf27fbf4ecc03fa122f3a9b139128e6e5648c
Crawford Vining <crawpdx@gmail.com>
2021-11-01 19:03:17 -0700

    added function to update object in array onclick

--- src/Board.js
+++ src/Board.js
@@ -2,29 +2,38 @@ import React, { useState } from 'react'
 // import Moves from './Moves'
 
 export default function Board(){
-    const [user, setUser] = useState(false)
-    // const [playerMoves, setPlayerMoves] = useState([Moves])
-
-    // console.log(playerMoves)
-
     const Moves = [
-        { square: 1, player: 'X', computer: 'O', empty: null },
-        { square: 2, player: 'X', computer: 'O', empty: null },
-        { square: 3, player: 'X', computer: 'O', empty: null },
-        { square: 4, player: 'X', computer: 'O', empty: null },
-        { square: 5, player: 'X', computer: 'O', empty: null },
-        { square: 6, player: 'X', computer: 'O', empty: null }, 
-        { square: 7, player: 'X', computer: 'O', empty: null },
-        { square: 8, player: 'X', computer: 'O', empty: null },
-        { square: 9, player: 'X', computer: 'O', empty: null }
+        { square: 1, player: 'X', computer: 'O', empty: true },
+        { square: 2, player: 'X', computer: 'O', empty: true },
+        { square: 3, player: 'X', computer: 'O', empty: true },
+        { square: 4, player: 'X', computer: 'O', empty: true },
+        { square: 5, player: 'X', computer: 'O', empty: true },
+        { square: 6, player: 'X', computer: 'O', empty: true }, 
+        { square: 7, player: 'X', computer: 'O', empty: true },
+        { square: 8, player: 'X', computer: 'O', empty: true },
+        { square: 9, player: 'X', computer: 'O', empty: true }
     ]
 
+    const [user, setUser] = useState(false)
+    const [playerMoves, setPlayerMoves] = useState(Moves)
+
+
+    console.log(playerMoves)
+    
     function handleMove(){
         if(user){
             document.getElementById('move').classList.add('show')
         } else {
             document.getElementById('move').classList.add('show')
         }
+    }
+
+    function updateSquare() {
+        setPlayerMoves([
+            {...playerMoves[8],
+            ...playerMoves[8],
+            empty: false
+        }])
     }
 
     return(
@@ -57,13 +66,13 @@ export default function Board(){
                 <span id="move" className="user-move">{user ? 'X' : '0'}</span>
             </div>
             <div>
-                <ul>
-                 {Moves.map(move => (
-                        <li style={{color:'#fff' }} key={move.square}>
-                            {move.player}, {move.computer}, {move.square}
-                        </li>
-                     )
-                 )}
+                <ul className="board-flex">
+                    {playerMoves.map(move => (
+                            <li id="square" onClick={updateSquare} style={{color:'#fff' }} key={move.square}>
+                                {move.player}, {move.computer}, {move.square}, {move.empty}
+                            </li>
+                        )
+                    )}
                 </ul>
             </div>
         </div>
