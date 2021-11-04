40ac1cbc83495e60553b269dbab0fdbd3870f42a
Crawford Vining <crawpdx@gmail.com>
2021-11-04 15:42:49 -0700

    updated array, sent arrary ot new file, connected function to squares

--- src/Board.js
+++ src/Board.js
@@ -1,80 +1,74 @@
 import React, { useState } from 'react'
-// import Moves from './Moves'
+import Moves from './Moves'
 
 export default function Board(){
-    const Moves = [
-        { square: 1, player: 'X', computer: 'O', empty: true },
-        { square: 2, player: 'X', computer: 'O', empty: true },
-        { square: 3, player: 'X', computer: 'O', empty: true },
-        { square: 4, player: 'X', computer: 'O', empty: true },
-        { square: 5, player: 'X', computer: 'O', empty: true },
-        { square: 6, player: 'X', computer: 'O', empty: true }, 
-        { square: 7, player: 'X', computer: 'O', empty: true },
-        { square: 8, player: 'X', computer: 'O', empty: true },
-        { square: 9, player: 'X', computer: 'O', empty: true }
-    ]
 
-    const [user, setUser] = useState(false)
-    const [playerMoves, setPlayerMoves] = useState(Moves)
+    // const [user, setUser] = useState(false)
+    // const [playerMoves, setPlayerMoves] = useState(Moves)
+    const [squares, setSquares] = useState(Array(9).fill(null))
 
+    // function handleClick(){
+    //     if(user){
+    //         document.getElementById('move').classList.add('show')
+    //     } else {
+    //         document.getElementById('move').classList.add('show')
+    //     }
+    // }
 
-    console.log(playerMoves)
-    
-    function handleMove(){
-        if(user){
-            document.getElementById('move').classList.add('show')
-        } else {
-            document.getElementById('move').classList.add('show')
-        }
-    }
+    // function updateSquare() {
+    //     setPlayerMoves([
+    //         {...playerMoves[8],
+    //         ...playerMoves[8],
+    //         empty: false
+    //     }])
+    // }
 
-    function updateSquare() {
-        setPlayerMoves([
-            {...playerMoves[8],
-            ...playerMoves[8],
-            empty: false
-        }])
-    }
+    function handleClick(i) {
+        let newSquares = squares
+        newSquares[i] = 'X';
+        setSquares(newSquares)
+        console.log(newSquares)
+      }
 
     return(
         <div className="board-flex">
-           <div id="square" onClick={handleMove}>
-                <span id="move" className="user-move">{user ? 'X' : '0'}</span>
+           <div id="square" onClick={() => handleClick(1)}>
+                <span id="move" className="user-move"></span>
             </div>
-            <div id="square" onClick={handleMove}>
-                <span id="move" className="user-move">{user ? 'X' : '0'}</span>
+            <div id="square" onClick={() => handleClick(2)}>
+                <span id="move" className="user-move"></span>
             </div>
-            <div id="square" onClick={handleMove}>
-                <span id="move" className="user-move">{user ? 'X' : '0'}</span>
+            <div id="square" onClick={() => handleClick(3)}>
+                <span id="move" className="user-move"></span>
             </div>
-            <div id="square" onClick={handleMove}>
-                <span id="move" className="user-move">{user ? 'X' : '0'}</span>
+            <div id="square" onClick={() => handleClick(4)}>
+                <span id="move" className="user-move"></span>
             </div>
-            <div id="square" onClick={handleMove}>
-                <span id="move" className="user-move">{user ? 'X' : '0'}</span>
+            <div id="square" onClick={() => handleClick(5)}>
+                <span id="move" className="user-move"></span>
             </div>
-            <div id="square" onClick={handleMove}>
-                <span id="move" className="user-move">{user ? 'X' : '0'}</span>
+            <div id="square" onClick={() => handleClick(6)}>
+                <span id="move" className="user-move"></span>
             </div>
-            <div id="square" onClick={handleMove}>
-                <span id="move" className="user-move">{user ? 'X' : '0'}</span>
+            <div id="square" onClick={() => handleClick(7)}>
+                <span id="move" className="user-move"></span>
             </div>
-            <div id="square" onClick={handleMove}>
-                <span id="move" className="user-move">{user ? 'X' : '0'}</span>
+            <div id="square" onClick={() => handleClick(8)}>
+                <span id="move" className="user-move"></span>
             </div>
-            <div id="square" onClick={handleMove}>
-                <span id="move" className="user-move">{user ? 'X' : '0'}</span>
+            <div id="square" onClick={() => handleClick(9)}>
+                <span id="move" className="user-move"></span>
             </div>
-            <div>
+            {/* <div>
                 <ul className="board-flex">
                     {playerMoves.map(move => (
-                            <li id="square" onClick={updateSquare} style={{color:'#fff' }} key={move.square}>
+                            <li id="square" onClick={handleClick} style={{color:'#fff' }} >
                                 {move.player}, {move.computer}, {move.square}, {move.empty}
                             </li>
                         )
                     )}
                 </ul>
-            </div>
+            </div> */}
         </div>
     )
 }
--- src/Moves.js
+++ src/Moves.js
@@ -1,11 +1,17 @@
 const Moves = [
-    { square: 1, player: 'X', computer: 'O', empty: null },
-    { square: 2, player: 'X', computer: 'O', empty: null },
-    { square: 3, player: 'X', computer: 'O', empty: null },
-    { square: 4, player: 'X', computer: 'O', empty: null },
-    { square: 5, player: 'X', computer: 'O', empty: null },
-    { square: 6, player: 'X', computer: 'O', empty: null }, 
-    { square: 7, player: 'X', computer: 'O', empty: null },
-    { square: 8, player: 'X', computer: 'O', empty: null },
-    { square: 9, player: 'X', computer: 'O', empty: null }
+    'X', 
+    null,
+    'O',
+    false
+    // { square: 1, player: 'X', computer: 'O', empty: null },
+    // { square: 2, player: 'X', computer: 'O', empty: null },
+    // { square: 3, player: 'X', computer: 'O', empty: null },
+    // { square: 4, player: 'X', computer: 'O', empty: null },
+    // { square: 5, player: 'X', computer: 'O', empty: null },
+    // { square: 6, player: 'X', computer: 'O', empty: null }, 
+    // { square: 7, player: 'X', computer: 'O', empty: null },
+    // { square: 8, player: 'X', computer: 'O', empty: null },
+    // { square: 9, player: 'X', computer: 'O', empty: null }
 ]
+
+export default Moves
