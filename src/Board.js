b0f534888c3f75304fcba7ce4459f48fc4111cee
Crawford Vining <crawpdx@gmail.com>
2021-11-12 16:37:55 -0800

    tried to figureout how to disable each button after click

--- src/Board.js
+++ src/Board.js
@@ -4,6 +4,7 @@ import Moves from './Moves'
 export default function Board(){
     const [squares, setSquares] = useState(Array(9).fill(null))
     const [player, setPlayer] = useState('X')
+    const [disabled, setDisabled] = useState(false)
 
     const getNextPlayer = (player) => {
         return player === 'X' ? setPlayer('0') : setPlayer('X')
@@ -14,39 +15,40 @@ export default function Board(){
         newSquares[i] = player;
         setSquares(newSquares)
         getNextPlayer(player)
+        setDisabled(true)
     }
 
     
 
     return(
         <div className="board-flex">
-           <div className="square" onClick={() => handleClick(0, player)}>
+           <button className="square" disabled={disabled} onClick={() => handleClick(0, player)}>
                 <span id="move" className="user-move">{squares[0]}</span>
-            </div>
-            <div className="square" onClick={() => handleClick(1, player)}>
+            </button>
+            <button className="square" disabled={disabled} onClick={() => handleClick(1, player)}>
                 <span id="move" className="user-move">{squares[1]}</span>
-            </div>
-            <div className="square" onClick={() => handleClick(2, player)}>
+            </button>
+            <button className="square" disabled={disabled} onClick={() => handleClick(2, player)}>
                 <span id="move" className="user-move">{squares[2]}</span>
-            </div>
-            <div className="square" onClick={() => handleClick(3, player)}>
+            </button>
+            <button className="square" disabled={disabled} onClick={() => handleClick(3, player)}>
                 <span id="move" className="user-move">{squares[3]}</span>
-            </div>
-            <div className="square" onClick={() => handleClick(4, player)}>
+            </button>
+            <button className="square" disabled={disabled} onClick={() => handleClick(4, player)}>
                 <span id="move" className="user-move">{squares[4]}</span>
-            </div>
-            <div className="square" onClick={() => handleClick(5, player)}>
+            </button>
+            <button className="square" disabled={disabled} onClick={() => handleClick(5, player)}>
                 <span id="move" className="user-move">{squares[5]}</span>
-            </div>
-            <div className="square" onClick={() => handleClick(6, player)}>
+            </button>
+            <button className="square" disabled={disabled} onClick={() => handleClick(6, player)}>
                 <span id="move" className="user-move">{squares[6]}</span>
-            </div>
-            <div className="square" onClick={() => handleClick(7, player)}>
+            </button>
+            <button className="square" disabled={disabled} onClick={() => handleClick(7, player)}>
                 <span id="move" className="user-move">{squares[7]}</span>
-            </div>
-            <div className="square" onClick={() => handleClick(8, player)}>
+            </button>
+            <button className="square" disabled={disabled} onClick={() => handleClick(8, player)}>
                 <span id="move" className="user-move">{squares[8]}</span>
-            </div>
+            </button>
         </div>
     )
 }
